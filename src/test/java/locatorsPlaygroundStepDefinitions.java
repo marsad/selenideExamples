c4672f1b4f2b492e7c0de4dbad9a5cd9ed20b3d8
marsad <martha.sadowska@gmail.com>
2023-03-23 10:46:49 -0100

    Initial commit

---
+++ src/test/java/locatorsPlaygroundStepDefinitions.java
@@ -0,0 +1,39 @@
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
+import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
+import org.openqa.selenium.By;
+
+import com.codeborne.selenide.Configuration;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class locatorsPlaygroundStepDefinitions {
+    @Given("an open browser with page \"Find By Playground - Locator Examples\"")
+    public void openFindByPlayground() {
+        open("https://testpages.herokuapp.com/styled/find-by-playground-test.html");
+        sleep(500);
+    }
+
+    @When("a title is visible on the page")
+    public void findParagraphById() {
+        $("h1").shouldHave(text("Find By Playground - Locator Examples"));
+    }
+
+    @Then("the found paragraph should contain A letter")
+    public void paragraphShouldHaveletterA() {
+        $("#p1").shouldHave(text("This is a paragraph text"));
+    }
+
+    @Then("the found paragraph should contain F letter")
+    public void paragraphShouldHaveletterF() {
+        $(By.name("pName6")).shouldHave(text("This is f paragraph text"));
+    }
+}
---
+++ src/test/resources/locators-playground.feature
@@ -0,0 +1,14 @@
+
+Feature: Locators Playground
+
+  Scenario: user can find paragraph by id
+
+    Given an open browser with page "Find By Playground - Locator Examples"
+    When a title is visible on the page
+    Then the found paragraph should contain A letter
+
+  Scenario: user can find paragraph by class name
+
+    Given an open browser with page "Find By Playground - Locator Examples"
+    When a title is visible on the page
+    Then the found paragraph should contain F letter
